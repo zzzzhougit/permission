3be8e97ff6c99c084b98792e1e0069dd994f49b9
Yao.Zhou <yaozhou.msg@outlook.com>
2018-07-17 22:00:39 +0800

    .

--- src/main/java/com/yaozhou/permission/dao/SysAclMapper.java
+++ src/main/java/com/yaozhou/permission/dao/SysAclMapper.java
@@ -3,6 +3,7 @@ package com.yaozhou.permission.dao;
 import com.yaozhou.permission.model.SysAcl;
 
 public interface SysAclMapper {
+
     int insert(SysAcl record);
 
     int insertSelective(SysAcl record);
