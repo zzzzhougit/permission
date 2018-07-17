9a29c86b93aa52f2f4e2a60f0a5018e949e520aa
Yao.Zhou <yaozhou.msg@outlook.com>
2018-07-18 00:13:03 +0800

    .

--- src/main/java/com/yaozhou/permission/controllers/BaseController.java
+++ src/main/java/com/yaozhou/permission/controllers/BaseController.java
@@ -1,8 +1,33 @@
 package com.yaozhou.permission.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
 /**
  * @author Yao.Zhou
  * @since 2018/7/17 23:45
  */
 public class BaseController {
+
+    @Autowired
+    protected ApplicationContext applicationContext;
+
+    public <T> T popBean(Class<T> clazz) {
+        if (null == applicationContext) {
+
+            return null;
+        }
+
+        return applicationContext.getBean(clazz);
+    }
+
+    public <T> T popBean(String name, Class<T> clazz) {
+        if (null == applicationContext) {
+
+            return null;
+        }
+
+        return applicationContext.getBean(name, clazz);
+    }
+
 }
