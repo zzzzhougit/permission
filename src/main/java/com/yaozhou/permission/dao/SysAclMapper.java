d6330c151d80c4a6004d9e7dff1091b0f6ea8944
Yao.Zhou <yaozhou.msg@outlook.com>
2018-07-17 23:43:02 +0800

    .

--- src/main/java/com/yaozhou/permission/dao/SysAclMapper.java
+++ src/main/java/com/yaozhou/permission/dao/SysAclMapper.java
@@ -3,7 +3,6 @@ package com.yaozhou.permission.dao;
 import com.yaozhou.permission.model.SysAcl;
 
 public interface SysAclMapper {
-
     int insert(SysAcl record);
 
     int insertSelective(SysAcl record);
--- src/main/java/com/yaozhou/permission/dao/SysDeptMapper.java
+++ src/main/java/com/yaozhou/permission/dao/SysDeptMapper.java
@@ -3,7 +3,6 @@ package com.yaozhou.permission.dao;
 import com.yaozhou.permission.model.SysDept;
 
 public interface SysDeptMapper {
-
     int insert(SysDept record);
 
     int insertSelective(SysDept record);
