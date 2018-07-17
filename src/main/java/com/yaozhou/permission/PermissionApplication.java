4b6a1e959f0f7a5d9ffb491f6142d3ff3c14fd01
yaozhou <yao.zhou.msg@gmail.com>
2018-07-17 13:11:40 +0800

    .

--- src/main/java/com/yaozhou/permission/PermissionApplication.java
+++ src/main/java/com/yaozhou/permission/PermissionApplication.java
@@ -3,10 +3,13 @@ package com.yaozhou.permission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Yao.Zhou
+ */
 @SpringBootApplication
 public class PermissionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PermissionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionApplication.class, args);
+    }
 }
