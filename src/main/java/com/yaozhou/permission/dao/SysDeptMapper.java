c2ad9324692d1347a2bf8f33c276332256496b20
Yao.Zhou <yaozhou.msg@outlook.com>
2018-07-17 22:01:52 +0800

    .

--- src/main/java/com/yaozhou/permission/dao/SysDeptMapper.java
+++ src/main/java/com/yaozhou/permission/dao/SysDeptMapper.java
@@ -3,6 +3,7 @@ package com.yaozhou.permission.dao;
 import com.yaozhou.permission.model.SysDept;
 
 public interface SysDeptMapper {
+
     int insert(SysDept record);
 
     int insertSelective(SysDept record);
